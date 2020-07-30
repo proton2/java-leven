3fa6e3339f12366ff9f45f14eff3a12497490ad1
proton2 <proton2@mail.ru>
2020-07-30 22:09:35 +0300

    Compute shader test

--- src/modules/ComputeBuffer.java
+++ src/modules/ComputeBuffer.java
@@ -3,7 +3,6 @@ package modules;
 import core.utils.BufferUtil;
 import org.lwjgl.opengl.GL30;
 
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -34,58 +33,48 @@ public class ComputeBuffer {
 	public void bindBufferBase(int index) {
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, ssbo);
 	}
-
-	public void unmapBuffer(){
-		glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-	}
 	
 	public void setData(int[] data) {
 		intBuffer = BufferUtil.createFlippedBuffer(data);
 		dataSize = data.length;
-		setData(intBuffer);
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+		glBufferData(GL_SHADER_STORAGE_BUFFER, intBuffer, GL_DYNAMIC_COPY);
 	}
 
 	public void setData(float[] data) {
 		floatBuffer = BufferUtil.createFlippedBuffer(data);
 		dataSize = data.length;
-		setData(floatBuffer);
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+		glBufferData(GL_SHADER_STORAGE_BUFFER, floatBuffer, GL_DYNAMIC_COPY);
 	}
 
 	public void setData(byte[] data) {
 		byteBuffer = BufferUtil.createFlippedBuffer(data);
 		dataSize = data.length;
-		setData(byteBuffer);
-	}
-
-	private void setData(IntBuffer intBuffer) {
 		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-		glBufferData(GL_SHADER_STORAGE_BUFFER, intBuffer, GL_DYNAMIC_COPY);
-	}
-
-	private void setData(FloatBuffer floatBuffer) {
-		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-		glBufferData(GL_SHADER_STORAGE_BUFFER, floatBuffer, GL_DYNAMIC_COPY);
-	}
-
-	private void setData(ByteBuffer data) {
-		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-		glBufferData(GL_SHADER_STORAGE_BUFFER, data, GL_STATIC_READ);
+		glBufferData(GL_SHADER_STORAGE_BUFFER, byteBuffer, GL_STATIC_READ);
 	}
 
 	public void getData(float[] data){
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
 		ByteBuffer buffer = GL30.glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, dataSize * 4, GL_MAP_READ_BIT);
 		FloatBuffer fb = buffer.asFloatBuffer();
 		fb.get(data);
+		glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	}
 
 	public void getData(int[] data){
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
 		ByteBuffer buffer = GL30.glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, dataSize * 4, GL_MAP_READ_BIT);
 		IntBuffer fb = buffer.asIntBuffer();
 		fb.get(data);
+		glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	}
 
 	public void getData(byte[] data){
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
 		ByteBuffer buffer = GL30.glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, dataSize, GL_MAP_READ_BIT);
 		buffer.get(data);
+		glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 	}
 }
--- src/modules/ComputeShaderTest.java
+++ src/modules/ComputeShaderTest.java
@@ -1,38 +1,40 @@
 package modules;
 
-import core.utils.BufferUtil;
-import org.lwjgl.opengl.GL30;
-
-import java.nio.*;
-
-import static org.lwjgl.opengl.GL30.*;
-import static org.lwjgl.opengl.GL43.GL_SHADER_STORAGE_BUFFER;
-import static org.lwjgl.opengl.GL43.glDispatchCompute;
+import java.util.Random;
 
 public class ComputeShaderTest {
 	private int N;
 	private ComputeShader shader;
-	private ComputeBuffer computeBuffer;
+	private ComputeBuffer computeBuffer, computeBufferPerm;
 	private int DATA_SIZE = 64 + 1;
 	private float[] floatArray;
 	private int[] intArray = new int[DATA_SIZE * DATA_SIZE * DATA_SIZE];
+	static int[] permutations = new int[512];
 
 	public ComputeShaderTest(int N) {
 		this.N = N;
+		InitPermutations(1200);
 
 		computeBuffer = new ComputeBuffer();
 		computeBuffer.setData(intArray);
+
+		computeBufferPerm = new ComputeBuffer();
+		computeBufferPerm.setData(permutations);
+
 		shader = ComputeShader.getInstance();
 	}
 	
 	public void render() {
 		shader.bind();
+
 		computeBuffer.bindBufferBase(0);
+		computeBufferPerm.bindBufferBase(1);
+
 		shader.updateUniforms(N);
 		shader.dispatch(1,1,1);
-		computeBuffer.getData(intArray);
 
-		computeBuffer.unmapBuffer();
+		computeBuffer.getData(intArray);
+		computeBufferPerm.getData(permutations);
 	}
 
 	private int[] initIntData(){
@@ -59,5 +61,15 @@ public class ComputeShaderTest {
 			}
 		}
 		return dataArray;
+	}
+
+	public static void InitPermutations(int seed)
+	{
+		Random random = new Random(seed);
+		for (int i = 0; i < 256; i++)
+			permutations[i] = (int) (256 * (random.nextInt(10000) / 10000.0f));
+
+		for (int i = 256; i < 512; i++)
+			permutations[i] = permutations[i - 256];
 	}
 }
