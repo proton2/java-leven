00a3f8d6bb941917d41ca0b395261b9d20acc208
proton2 <proton2@mail.ru>
2020-12-26 14:08:17 +0300

    refactoring

--- src/dc/ChunkOctreeWrapper.java
+++ src/dc/ChunkOctreeWrapper.java
@@ -50,8 +50,8 @@ public class ChunkOctreeWrapper extends GameObject {
         //VoxelOctree voxelOctree = new PointerBasedOctreeImpl(true, meshGenCtx);
         //VoxelOctree voxelOctree = new SimpleLinearOctreeImpl(meshGenCtx);
         //VoxelOctree voxelOctree = new TransitionLinearOctreeImpl(meshGenCtx);
-        VoxelOctree voxelOctree = new LevenLinearCPUOctreeImpl(meshGenCtx);
-        //VoxelOctree voxelOctree = new LevenLinearGPUOctreeImpl(kernelHolder, meshGenCtx, ctx);
+        //VoxelOctree voxelOctree = new LevenLinearCPUOctreeImpl(meshGenCtx);
+        VoxelOctree voxelOctree = new LevenLinearGPUOctreeImpl(kernelHolder, meshGenCtx, ctx);
         chunkOctree = new ChunkOctree(voxelOctree, meshGenCtx);
     }
 
