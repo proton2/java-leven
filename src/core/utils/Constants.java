8d3c07f290e96e86f5ca0a28ea05121fae742112
proton2 <proton2@mail.ru>
2021-05-08 23:30:14 +0300

    draw frustum planes for debug

--- src/core/utils/Constants.java
+++ src/core/utils/Constants.java
@@ -5,7 +5,7 @@ import core.math.Vec3f;
 public class Constants {
 
 	public static final long NANOSECOND = 1000000000;
-	public static final float ZFAR = 10000.0f;
+	public static final float ZFAR = 600.0f;
 	public static final float ZNEAR = 0.1f;
 	
 	public static final String RENDERER_COMPONENT = "Renderer";
